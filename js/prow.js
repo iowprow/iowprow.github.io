dd9234659b09bfc9d9c0a936b58d2ab486d4b34a
iowprow <160417146+iowprow@users.noreply.github.com>
2024-02-29 18:12:39 +0000

    added search function

--- js/prow.js
+++ js/prow.js
@@ -37,4 +37,45 @@
 		}
 	}	
 	
+	var uniqueLayernames=[];	
+	var arr = [];
+	var arr1 = [];
+
+	function forEachFeature2(feature, layer) {		
+		layer.on({
+			mouseover: highlightFeature,
+			mouseout: resetHighlight,
+		});			   
+		if (feature.properties && feature.properties.Name) {
+			var tooltipname = feature.properties.Name.replace('|','');
+			
+			if (feature.properties.ROW_TYPE == "Footpath") {
+				layerName = feature.properties.PARISH+"_fo";
+
+			} else if (feature.properties.ROW_TYPE == "BOAT") {
+				layerName = feature.properties.PARISH+"_bo";
+
+			} else if (feature.properties.ROW_TYPE == "Bridleway") {
+				layerName = feature.properties.PARISH+"_br";
+			}
+
+			lat = feature.geometry.coordinates[0][0];
+			lon = feature.geometry.coordinates[0][1];
+			latlon = +lat+","+lon+","+layerName;
+			
+			// just pass in one layer if there are multiple
+			if(!uniqueLayernames.includes(tooltipname)) {
+				arr1.push({label:tooltipname, value:latlon });
+			} 
+			uniqueLayernames.push(tooltipname);
+				
+			layer.bindTooltip(tooltipname, {
+				direction: "center",
+				permanent: true, 
+				opacity: 0.9,
+				className: 'tooltipStyle'
+			});
+		}
+	}	
+	
 	
